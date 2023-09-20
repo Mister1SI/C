0831a8967dd8162da927038f69d61a2986ddfedc
Mister1SI <wazzup2690@gmail.com>
2023-09-20 16:24:37 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "h/shifter.h"
+
+void help();
+
+int main(int argc, char* argv[]) {
+    FILE* file;
+    char buffer[1024];
+    if (argc < 2) {
+        help();
+        return 0;
+    }
+
+    return 0;
+}
+
+void help() {
+
+
+    const char* help_text = 
+"\nCOOLCIPHER\n\
+===========\n\
+HELP MENU\n\
+\n\
+usage: coolcipher <cipher> <mode> <input type> <input>\n\
+\n\
+cipher:\n\
+    -c: Caesar cipher\n\
+\n\
+";
+
+
+    printf(help_text);
+}
